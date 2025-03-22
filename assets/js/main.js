617234368258d9e8a59e066371bdfe74d5f3de39
host-BloodCloud <bloodcloudcontact@gmail.com>
2025-03-22 13:52:03 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
+import { account } from './appwrite';
+
 // Variable Declaration
 const loginBtn = document.querySelector("#login");
 const registerBtn = document.querySelector("#register");
 const loginForm = document.querySelector(".login-form");
 const registerForm = document.querySelector(".register-form");
+const prodfileScreen = document.getElementById("prodfileScreen");
+const loginsScreen = document.getElementById("loginsScreen");
+const googleLoginBtn = document.getElementById("google-login");
 
 // Login button function
-loginBtn.addEventListener("click", () => {
+loginBtn.addEventListener('click', () => {
     loginBtn.style.backgroundColor = "#21264D";
     registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
 
@@ -18,7 +23,7 @@ loginBtn.addEventListener("click", () => {
     document.querySelector(".col-1").style.borderRadius = "0 30% 20% 0";
 });
 
-registerBtn.addEventListener("click", () => {
+registerBtn.addEventListener('click', () => {
     loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
     registerBtn.style.backgroundColor = "#21264D";
 
@@ -31,41 +36,12 @@ registerBtn.addEventListener("click", () => {
     document.querySelector(".col-1").style.borderRadius = "0 20% 30% 0";
 });
 
-// Signup & Signin Functionality
-document.addEventListener("DOMContentLoaded", function () {
-    // Get buttons
-    const signUpBtn = document.querySelector(".register-form .input-submit");
-    const signInBtn = document.querySelector(".login-form .input-submit");
+async function handleLogin() {
+    account.createOAuth2Session(
+        'google',
+        'https://host-bloodcloud.github.io/',
+        'https://host-bloodcloud.github.io/failoauth2'
+    );
+}
 
-    signUpBtn.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent form submission
-
-        let email = document.querySelector(".register-form input[type='email']").value;
-        let username = document.querySelector(".register-form input[type='username']").value;
-        let password = document.querySelector(".register-form input[type='password']").value;
-
-        if (username && password && email) {
-            localStorage.setItem("savedUsername", username);
-            localStorage.setItem("savedPassword", password);
-            alert("Account created successfully!");
-        } else {
-            alert("Please enter all details.");
-        }
-    });
-
-    signInBtn.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent form submission
-
-        let enteredUsername = document.querySelector(".login-form input[type='username']").value;
-        let enteredPassword = document.querySelector(".login-form input[type='password']").value;
-
-        let savedUsername = localStorage.getItem("savedUsername");
-        let savedPassword = localStorage.getItem("savedPassword");
-
-        if (enteredUsername === savedUsername && enteredPassword === savedPassword) {
-            window.location.href = "https://host-bloodcloud.github.io/home";
-        } else {
-            alert("Incorrect username or password!");
-        }
-    });
-});
+googleLoginBtn.addEventListener("click", handleLogin);
